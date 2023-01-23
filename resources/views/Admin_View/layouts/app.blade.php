a87e048b3b5d4c59d4db770d8633d7a54d874bd9
xplct-cont <kennsecusana@gmail.com>
2023-01-23 13:29:29 +0800

    re-layout

--- resources/views/Admin_View/layouts/app.blade.php
+++ resources/views/Admin_View/layouts/app.blade.php
@@ -31,12 +31,12 @@
           href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css"
           integrity="sha512-aEe/ZxePawj0+G2R+AaIxgrQuKT68I28qh+wgLrcAJOz3rxCP+TwrK5SPN+E5I+1IQjNtcfvb96HDagwrKRdBw=="
           crossorigin="anonymous"/>
-
+          @livewireStyles
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     @yield('third_party_stylesheets')
 
     @stack('page_css')
-    @livewireStyles
+    
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
