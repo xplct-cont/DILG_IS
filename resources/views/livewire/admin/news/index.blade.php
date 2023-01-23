94232d8f1208cf3e092b312012a84d3cf0a2a901
viennarose <viennapepito01@gmail.com>
2023-01-23 11:26:18 +0800

    livewire

---
+++ resources/views/livewire/admin/news/index.blade.php
@@ -0,0 +1,27 @@
+<div>
+    @include('livewire.admin.news.create')
+    <div class="card mt-5">
+        <div class="card-header d-flex">
+            <h1>News</h1>
+            <button type="button" class="btn btn-primary col-md-2 ms-auto" data-bs-toggle="modal" data-bs-target="#NewsModal">
+            Add News</button>
+        </div>
+    <div>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Caption</th>
+                <th scope="col">Image</th>
+                <th scope="col">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr></tr>
+              <tr></tr>
+              <tr></tr>
+            </tbody>
+          </table>
+    </div>
+</div>
