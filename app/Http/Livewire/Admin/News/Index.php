9ce7ff76cdc50472996083472277ea7925d26c2c
viennarose <viennapepito01@gmail.com>
2023-01-23 13:21:09 +0800

    livewire

--- app/Http/Livewire/Admin/News/Index.php
+++ app/Http/Livewire/Admin/News/Index.php
@@ -3,12 +3,22 @@
 namespace App\Http\Livewire\Admin\News;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Index extends Component
 {
-    public $title, $caption, $image;
+    use WithFileUploads;
+    public $title, $caption, $images = [];
 
+    public function addNews(){
+        $this->validate([
+            'image.*' => 'image', // 1MB Max
+        ]);
 
+        foreach ($this->image as $img) {
+            $img->store('image');
+        }
+    }
     public function render()
     {
         return view('livewire.admin.news.index');
--- resources/views/Admin_View/layouts/app.blade.php
+++ resources/views/Admin_View/layouts/app.blade.php
@@ -150,7 +150,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.3.4/js/bootstrap-switch.min.js" integrity="sha512-J+763o/bd3r9iW+gFEqTaeyi+uAphmzkE/zU8FxY6iAvD3nQKXa+ZAWkBI9QS9QkYEKddQoiy0I5GDxKf/ORBA==" crossorigin="anonymous"></script>
-
+@livewireScripts
 <script>
     $(function () {
         bsCustomFileInput.init();
@@ -162,7 +162,7 @@
 </script>
 
 @yield('third_party_scripts')
-@livewireScripts
+
 @stack('page_scripts')
 </body>
 </html>
