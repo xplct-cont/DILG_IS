8d30bbb34b88c98a0a31cd17b0df1daadd0370eb
viennarose <viennapepito01@gmail.com>
2023-01-23 13:22:41 +0800

    livewire

--- resources/views/Admin_View/layouts/app.blade.php
+++ resources/views/Admin_View/layouts/app.blade.php
@@ -34,8 +34,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     @yield('third_party_stylesheets')
-    @livewireStyles
+
     @stack('page_css')
+    @livewireStyles
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -103,7 +104,7 @@
                 </div>
             </div>
         </footer>
-       </div> 
+       </div>
 
 </div>
 
