6924b1e1b58f072701a9d7d9214c2b9f677f3d52
xplct-cont <kennsecusana@gmail.com>
2023-01-26 15:40:18 +0800

    Retrieved jobs data to normal_view

--- resources/views/Normal_View/Jobs/index.blade.php
+++ resources/views/Normal_View/Jobs/index.blade.php
@@ -1,105 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    
-    <div class="header  ml-3 mr-3" style=" padding: 7px; background: #002c76; color: #FFFFFF; margin-bottom: 5px;">
-        <h1 class="text-center" style="font-size: 18px; margin-top: 10px; ">JOB VACANCIES</h1>
-    </div>
+    <div>
 
-    <div class="row mx-auto">
-        <div class="card border border-0">
-            <div class="card-body mx-auto">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <img style="width: 100%; height: auto;" src="{{ asset('/img/hiring.jpg') }}">
-                    </div>
-                    <div class="col-sm-8 text-center">
-                        <h2 class="text-center" style="font-size: 25px;">We are hiring the following positions:</h2>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Planning Officer</li>
-                            <li class="list-group-item">Engineers</li>
-                            <li class="list-group-item">Developers</li>
-                          </ul>
-                        <a href="#" class="btn text-white col-sm-2 mx-auto text-center" style="background-color: #C9282D">View Details</a>
+        <div class="header  ml-3 mr-3" style=" padding: 7px; background: #002c76; color: #FFFFFF; margin-bottom: 5px;">
+            <h1 class="text-center" style="font-size: 18px; margin-top: 10px; ">JOB VACANCIES</h1>
+        </div>
+
+        @foreach ($jobs as $job_list)
+            <div class="row mx-auto">
+                <div class="card border border-0">
+                    <div class="card-body mx-auto">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <img style="width: 100%; height: auto;"
+                                    src="{{ asset('storage/hiring_images/' . $job_list->hiring_img) }} ">
+                            </div>
+                            <div class="col-sm-8 text-center">
+                                <h2 class="text-center" style="font-size: 25px;">We are hiring the following positions:</h2>
+                                <h3 class="text-center mt-4" style="font-size: 22px; font-weight: 400;">
+                                    {{ $job_list->position }}</h3>
+                                <p class="text-justify mt-5" style="font-size: 18px; ">Description:</p>
+                                <p class="text-justify" style="font-size: 16px; font-weight: 300; margin-top: -12px;">
+                                    {{ $job_list->details }}</p>
+                                <a href="{{ $job_list->id }}"
+                                    class=" mt-5 btn btn-sm text-white col-sm-2 mx-auto text-center"
+                                    style="background-color: #C9282D">View Details</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
-    <div class="row mx-auto">
-        <div class="card border border-0">
-            <div class="card-body mx-auto">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <img style="width: 100%; height: auto;" src="{{ asset('/img/hiring.jpg') }}">
-                    </div>
-                    <div class="col-sm-8 text-center">
-                        <h2 class="text-center" style="font-size: 25px;">We are hiring the following positions:</h2>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Planning Officer</li>
-                            <li class="list-group-item">Engineers</li>
-                            <li class="list-group-item">Developers</li>
-                          </ul>
-                        <a href="#" class="btn text-white col-sm-2 mx-auto text-center" style="background-color: #C9282D">View Details</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mx-auto">
-        <div class="card border border-0">
-            <div class="card-body mx-auto">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <img style="width: 100%; height: auto;" src="{{ asset('/img/hiring.jpg') }}">
-                    </div>
-                    <div class="col-sm-8 text-center">
-                        <h2 class="text-center" style="font-size: 25px;">We are hiring the following positions:</h2>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Planning Officer</li>
-                            <li class="list-group-item">Engineers</li>
-                            <li class="list-group-item">Developers</li>
-                          </ul>
-                        <a href="#" class="btn text-white col-sm-2 mx-auto text-center" style="background-color: #C9282D">View Details</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mx-auto">
-        <div class="card border border-0">
-            <div class="card-body mx-auto">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <img style="width: 100%; height: auto;" src="{{ asset('/img/hiring.jpg') }}">
-                    </div>
-                    <div class="col-sm-8 text-center">
-                        <h2 class="text-center" style="font-size: 25px;">We are hiring the following positions:</h2>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Planning Officer</li>
-                            <li class="list-group-item">Engineers</li>
-                            <li class="list-group-item">Developers</li>
-                          </ul>
-                        <a href="#" class="btn text-white col-sm-2 mx-auto text-center" style="background-color: #C9282D">View Details</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    {{-- <div class="row mt-2">
-        <div class="col-md-12">
-          <div class="card border border-0">
-            <div class="card-body d-flex">
-            <img style=" width: 30%; height: auto;" src="{{ asset('/img/hiring.jpg') }}">
-              <h6 style="font-size:3vw;" class="ms-5 text-justify">We are hiring the following positions:</h6>
-              <a href="#" class="btn btn-dark text-white">View Details</a>
-            </div>
-
-          </div>
-        </div>
-      </div> --}}
-</div>
 @endsection
