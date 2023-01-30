88079265e63080d2f666e3804b3b082da8d80768
xplct-cont <kennsecusana@gmail.com>
2023-01-30 16:42:09 +0800

    Added layout

--- resources/views/Normal_View/Home/home.blade.php
+++ resources/views/Normal_View/Home/home.blade.php
@@ -75,8 +75,41 @@
         <h1 class="text-center" style="font-size: 18px; margin-top: 10px;">NEWS & UPDATES</h1>
     </div>
 
-
     <div class="mx-auto col-md-11 d-flex align-items-center justify-content-around mt-5">
+        <div class="row m-1">
+
+            @php $show = DB::table('news')->limit(3)->get(); @endphp
+            @foreach ($show as $new)
+                <a href="#" style="text-decoration: none; color:#030303;">
+                    <div class="card elevation-4">
+                        <div class="row d-flex justify-content-center mt-3 ml-3 mr-3  ">
+                            <div class="col-md-6 mx-auto mt-5 ">
+
+                                <h1 style="font-size: 20px;">
+                                    Post: {{ \Carbon\Carbon::parse($new->datetime)->format('M j, Y h:i a') }}</h1><br>
+                                <p class="text-left" style="font-size: 18px;">{{ $new->title }}</p>
+                                <p class="text-left" style="font-size: 15px; font-weight: 400;"> {{ $new->caption }}</p>
+                            </div>
+                            <div class="col-md-6">
+                                <div class=" d-flex flex-row align-items-center justify-content-around "
+                                    style="height: 400px;">
+                                    @php $images = json_decode($new->image,true); @endphp
+                                    @if (is_array($images) && !empty($images))
+                                        @foreach ($images as $image)
+                                            <img src="{{ asset('app/public/' . $image) }}"
+                                                style="height:300px; width:auto;" />
+                                        @endforeach
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
+
+    {{-- <div class="mx-auto col-md-11 d-flex align-items-center justify-content-around mt-5">
         <div class="row m-1">
             <a href="#" style="text-decoration: none; color:#030303;">
                 <div class="card elevation-4">
@@ -150,7 +183,7 @@
                 </div>
             </a>
         </div>
-    </div>
+    </div> --}}
     </div>
     </div>
 
