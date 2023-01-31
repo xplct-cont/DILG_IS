adc227dd95f0229aaa0dfa47a0b6c016aebb7885
xplct-cont <kennsecusana@gmail.com>
2023-01-31 10:50:21 +0800

    Added count dashboard

--- resources/views/Admin_View/layouts/home.blade.php
+++ resources/views/Admin_View/layouts/home.blade.php
@@ -1,13 +1,103 @@
 @extends('Admin_View.layouts.app')
 
 @section('content')
-<div class="container-fluid">
-
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="mt-2" style="font-size:22px; font-weight: 400; color:dimgray;">Dashboard</h1>
+    <div class="text-start p-1 ml-1" style="font-size: 25px; font-weight: 500; color:dimgray;">
+        DASHBOARD
     </div>
-   
 
-</div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>{{ $news }}</h3>
+
+                            <p class="text-secondary">News</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-newspaper text-secondary"></i>
+                        </div>
+                        <a href="{{ route('admin/news') }}" class="small-box-footer text-light"
+                            style="background-color:dimgray;"><span class="text-light">More info</span> <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>0</h3>
+
+                            <p class="text-secondary">Projects</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-exclamation text-secondary"></i>
+                        </div>
+                        <a href="" class="small-box-footer"
+                            style="background-color:dimgray;color:whitesmoke;
+              "><span class="text-light">More
+                                info</span> <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>{{ $jobs }}</h3>
+
+                            <p class="text-secondary">Job Vacancies</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-address-book text-secondary"></i>
+                        </div>
+                        <a href="{{ route('admin/jobs') }}" class="small-box-footer "
+                            style="background-color:dimgray;color:whitesmoke;"><span class="text-light">More info</span> <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>{{ $orgs }}</h3>
+
+                            <p class="text-secondary">Organizational Structure</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-address-book text-secondary"></i>
+                        </div>
+                        <a href="{{ route('admin/organization') }}" class="small-box-footer "
+                            style="background-color:dimgray;color:whitesmoke;"><span class="text-light">More info</span> <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6 ">
+                    <!-- small box -->
+                    <div class="small-box text-light bg-info elevation-4">
+                        <div class="inner">
+                            <h3>{{ $pdmus }}</h3>
+
+                            <p class="text-light">PDMU</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users"></i>
+                        </div>
+                        <a href="{{ route('admin/pdmu') }}" class="small-box-footer " style="background-color:dimgray;">
+                            <span class="text-light">More info</span> <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    </div>
 @endsection
