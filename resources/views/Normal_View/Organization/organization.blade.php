f677567376be86cebfe2ab798c806afd7c5ea4b2
xplct-cont <kennsecusana@gmail.com>
2023-01-31 11:13:02 +0800

    Added effects

--- resources/views/Normal_View/Organization/organization.blade.php
+++ resources/views/Normal_View/Organization/organization.blade.php
@@ -9,14 +9,16 @@
 
     @foreach ($pd as $pdirector)
         <div class="d-flex justify-content-center mb-3">
-            <img src="{{ asset('org_profile_images/' . $pdirector->profile_img) }} " alt="Image"
-                class="shadow-lg border border-dark" style="border-radius: 50%; height: 300px; width: 300px;">
-        </div>
-        <p class="text-center" style="font-size: 18px; font-weight:500;">{{ $pdirector->fname }}
-            {{ $pdirector->mid_initial }} {{ $pdirector->lname }}</p>
-        <p class="text-center" style="margin-top: -20px; font-size: 13px;">{{ $pdirector->position }}</p>
-    @endforeach
+            <div class="zoom">
+                <img src="{{ asset('org_profile_images/' . $pdirector->profile_img) }} " alt="Image"
+                    class="shadow-lg border border-dark" style="border-radius: 50%; height: 300px; width: 300px;">
 
+                <p class="text-center" style="font-size: 18px; font-weight:500;">{{ $pdirector->fname }}
+                    {{ $pdirector->mid_initial }} {{ $pdirector->lname }}</p>
+                <p class="text-center" style="margin-top: -20px; font-size: 13px;">{{ $pdirector->position }}</p>
+    @endforeach
+    </div>
+    </div>
 
 
     <div class="col-md-12 mx-auto mb-3 mt-3">
@@ -25,7 +27,7 @@
 
             @foreach ($head as $heads)
                 <div class="col-md-6  d-flex justify-content-center">
-                    <div class="">
+                    <div class="zoom">
                         <img src="{{ asset('org_profile_images/' . $heads->profile_img) }} " alt="Image"
                             class="shadow-lg border border-dark" style="border-radius: 50%; height: 250px; width: 250px;">
                         <p class="text-center" style="font-size: 18px; font-weight:500;">{{ $heads->fname }}
@@ -41,16 +43,17 @@
 
     @foreach ($head_2 as $heads_2)
         <div class="d-flex justify-content-center mb-3">
-            <img src="{{ asset('org_profile_images/' . $heads_2->profile_img) }} " alt="Image"
-                class="shadow-lg border border-dark elevation-4" style="border-radius: 50%; height: 230px; width: 230px;">
-        </div>
-        <p class="text-center" style="font-size: 18px; font-weight:500;">{{ $heads_2->fname }}
-            {{ $heads_2->mid_initial }} {{ $heads_2->lname }}</p>
-        <p class="text-center" style="margin-top: -20px; font-size: 13px;">{{ $heads_2->position }}</p>
+            <div class="zoom">
+                <img src="{{ asset('org_profile_images/' . $heads_2->profile_img) }} " alt="Image"
+                    class="shadow-lg border border-dark elevation-4"
+                    style="border-radius: 50%; height: 230px; width: 230px;">
+
+                <p class="text-center" style="font-size: 18px; font-weight:500;">{{ $heads_2->fname }}
+                    {{ $heads_2->mid_initial }} {{ $heads_2->lname }}</p>
+                <p class="text-center" style="margin-top: -20px; font-size: 13px;">{{ $heads_2->position }}</p>
     @endforeach
-
-
-
+    </div>
+    </div>
 
     <div class="">
         <div class="col-md-12 mb-3 mt-3">
@@ -64,13 +67,13 @@
 
                         @foreach ($d_one as $d_ones)
                             <div class="col-md-4 text-center d-flex justify-content-center">
-                                <div class="">
+                                <div class="zoom">
                                     <img src="{{ asset('org_profile_images/' . $d_ones->profile_img) }} " alt="Image"
                                         class="shadow-lg border border-dark"
                                         style="border-radius: 50%; height: 150px; width: 150px;">
-                                    <p class="text-center" style="font-size: 18px; font-weight:500;">{{ $d_ones->fname }}
+                                    <p class="text-center" style=" font-weight:500;">{{ $d_ones->fname }}
                                         {{ $d_ones->mid_initial }} {{ $d_ones->lname }}</p>
-                                    <p class="text-center" style="margin-top: -20px; font-size: 13px;">
+                                    <p class="text-center" style="margin-top: -20px;">
                                         {{ $d_ones->position }}</p>
                                 </div>
                             </div>
@@ -83,13 +86,13 @@
 
                         @foreach ($admin_ser as $ad_ser)
                             <div class="col-md-4 text-center d-flex justify-content-center">
-                                <div class="">
+                                <div class="zoom">
                                     <img src="{{ asset('org_profile_images/' . $ad_ser->profile_img) }} " alt="Image"
                                         class="shadow-lg border border-dark"
                                         style="border-radius: 50%; height: 150px; width: 150px;">
-                                    <p class="text-center" style="font-size: 18px; font-weight:500;">{{ $ad_ser->fname }}
+                                    <p class="text-center" style=" font-weight:500;">{{ $ad_ser->fname }}
                                         {{ $ad_ser->mid_initial }} {{ $ad_ser->lname }}</p>
-                                    <p class="text-center" style="margin-top: -20px; font-size: 13px;">
+                                    <p class="text-center" style="margin-top: -20px; ">
                                         {{ $ad_ser->position }}</p>
                                 </div>
                             </div>
@@ -98,3 +101,20 @@
                 </div>
             </div>
         @endsection
+
+
+        <style scoped>
+            .zoom {
+                transition: transform .2s;
+            }
+
+            .zoom:hover {
+                -ms-transform: scale(1.5);
+                /* IE 9 */
+                -webkit-transform: scale(1.5);
+                /* Safari 3-8 */
+                transform: scale(1.5);
+                text-align: center;
+                font-size: 10px;
+            }
+        </style>
