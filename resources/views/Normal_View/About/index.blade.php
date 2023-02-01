b7f5b3ce70c6100d9bf740e465bea6ff4b961d8e
xplct-cont <kennsecusana@gmail.com>
2023-02-01 10:54:57 +0800

    Added About Data

--- resources/views/Normal_View/About/index.blade.php
+++ resources/views/Normal_View/About/index.blade.php
@@ -7,9 +7,9 @@
             <h1 class="text-center" style="font-size: 18px; margin-top: 10px;">ABOUT US</h1>
         </div>
 
-        <div class="container">
-            <h1>Who we are</h1>
-            <div class="container">
+        <div class="col-md-11 mx-auto mt-3">
+            {{-- <h1 style="font-size: 30px;">Who we are</h1> --}}
+            <div class="">
                 <ul class="text-justify list-group">
 
                     <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color:#002c76"></span> The
@@ -37,148 +37,59 @@
             </div>
         </div>
 
-        <div class="container">
-            <h1>Powers & Functions</h1>
-            <div class="container">
-                <ul class="text-justify list-group">
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color:#002c76"></span> Assist
-                        the
-                        President in the
-                        exercise of general supervision over local
-                        governments
-                    </li>
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color:#002c76"></span> Advise
-                        the
-                        President in the
-                        promulgation of policies, rules, regulations and
-                        other issuances on the
-                        general supervision over local governments and on public order and safety
-                    </li>
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color:#002c76"></span>
-                        Establish and prescribe rules, regulations and other issuances implementing laws on public order and
-                        safety, the general supervision over local governments and the promotion of local autonomy and
-                        community empowerment and monitor compliance thereof;
 
-                    </li>
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color:#002c76"></span> Provide
-                        assistance
-                        towards
-                        legislation regarding local governments, law
-                        enforcement and public
-                        safety;
-                    </li>
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color:#002c76"></span>
-                        Establish and
-                        prescribe
-                        plans, policies, programs and projects to promote
-                        peace and order, ensure
-                        public safety and further strengthen the administrative, technical and fiscal capabilities of local
-                        government offices and personnel;
-                    </li>
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color:#002c76"></span>
-                        Formulate plans,
-                        policies and
-                        programs which will meet local emergencies
-                        arising from natural and
-                        man-made disasters;
-                    </li>
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3 " style="color: #002c76"></span>
-                        Establish a system
-                        of
-                        coordination and cooperation among the citizenry,
-                        local executives and the
-                        Department, to ensure effective and efficient delivery of basic services to the public; and
-                    </li>
-                    <li class="list-group-item"> <span class="fas fa-arrow-right mr-3" style="color: #002c76"></span>
-                        Organize, train and
-                        equip
-                        primarily for the performance of police functions,
-                        a police force that is
-                        national in scope and civilian in character.
-                    </li>
-                </ul>
+        <div class="col-md-12 mx-auto mt-5">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="d-flex justify-content-center">
+                        <div class="col-md-1 mx-auto">
+                            <img src="img/goal.png" style="height: 150px;" alt="">
+                        </div>
+                        <div class="col-md-5 mx-auto">
+                            <h1 class="text-center" style="font-size: 22px;">Our Mission</h1>
+                            <p class="text-justify">The Department shall ensure peace and order, public safety and security,
+                                uphold excellence in local governance and enable resilient and inclusive communities.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+
+                    <div class="d-flex justify-content-center">
+                        <div class="col-md-1 mx-auto">
+                            <img src="img/lightbulb.png" style="height: 150px;" alt="">
+                        </div>
+                        <div class="col-md-5 mx-auto">
+                            <h1 class="text-center" style="font-size: 22px;">Our Vision</h1>
+                            <p class="text-justify">A highly trusted Department and Partner in nurturing local governments
+                                and sustaining peaceful, safe, progressive, resilient, and inclusive communities towards a
+                                comfortable and secure life for Filipinos by 240.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="container d-flex align-items-center justify-content-around mt-5">
-            <div class="row m-1">
-                <a href="#" style="text-decoration: none; color:#030303;">
-                    <div class="card elevation-4">
-                        <div class="row d-flex justify-content-center mt-3 ml-3 mr-3  ">
-                            <div class="col-md-6 ">
-                                <h1 style="font-size: 15px;">December 15, 2022</h1><br>
-                                <p class="text-left" style="font-size: 16px;">DILG conferred the Seal of Good Local
-                                    Governance
-                                    (SGLG) Award to LGU Alburquerque </p>
-                                <p class="text-left" style="font-size: 13px; font-weight: 300;"> The activity was also
-                                    attended by DILG 7 Regional Director Leocadio Trovela, CESO III, Assistant Regional
-                                    Director
-                                    Maria Loisella Lucino, CESO IV, DILG Bohol Provincial Director Jerome Gonzales, LGMED
-                                    Chief
-                                    Celerino Magto Jr. and SGLG Regional and Provincial Focal Persons.</p>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="card" style="height: 300px;">
-                                    <img src="{{ asset('/img/img3.jpg') }}" alt=""
-                                        style="height: 300px; width:auto;">
-                                </div>
-                            </div>
-                        </div>
+        <div class="col-md-6 mx-auto">
+
+            <div class="d-flex justify-content-center">
+                <div class="col-md-10 mx-auto">
+                    <div class="text-center">
+                        <img class="" src="img/star.png" style="height: 150px;" alt="">
                     </div>
-                </a>
-                <a href="#" style="text-decoration: none; color:#030303;">
-                    <div class="card elevation-4">
-                        <div class="row d-flex justify-content-center mt-3 ml-3 mr-3  ">
-                            <div class="col-md-6 ">
-                                <h1 style="font-size: 15px;">December 15, 2022</h1><br>
-                                <p class="text-left" style="font-size: 16px;">DILG conferred the Seal of Good Local
-                                    Governance
-                                    (SGLG) Award to LGU Alburquerque </p>
-                                <p class="text-left" style="font-size: 13px; font-weight: 300;"> The activity was also
-                                    attended by DILG 7 Regional Director Leocadio Trovela, CESO III, Assistant Regional
-                                    Director
-                                    Maria Loisella Lucino, CESO IV, DILG Bohol Provincial Director Jerome Gonzales, LGMED
-                                    Chief
-                                    Celerino Magto Jr. and SGLG Regional and Provincial Focal Persons.</p>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="card" style="height: 300px;">
-                                    <img src="{{ asset('/img/img3.jpg') }}" alt=""
-                                        style="height: 300px; width:auto;">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" style="text-decoration: none; color:#030303;">
-                    <div class="card elevation-4">
-                        <div class="row d-flex justify-content-center mt-3 ml-3 mr-3  ">
-                            <div class="col-md-6 ">
-                                <h1 style="font-size: 15px;">December 15, 2022</h1><br>
-                                <p class="text-left" style="font-size: 16px;">DILG conferred the Seal of Good Local
-                                    Governance
-                                    (SGLG) Award to LGU Alburquerque </p>
-                                <p class="text-left" style="font-size: 13px; font-weight: 300;"> The activity was also
-                                    attended by DILG 7 Regional Director Leocadio Trovela, CESO III, Assistant Regional
-                                    Director
-                                    Maria Loisella Lucino, CESO IV, DILG Bohol Provincial Director Jerome Gonzales, LGMED
-                                    Chief
-                                    Celerino Magto Jr. and SGLG Regional and Provincial Focal Persons.</p>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="card" style="height: 300px;">
-                                    <img src="{{ asset('/img/img3.jpg') }}" alt=""
-                                        style="height: 300px; width:auto;">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
+                    <h1 class="text-center" style="font-size: 22px;">Our Goals</h1>
+                    <p class="text-justify">Peaceful, safe, self-reliant and development-dominated communities. Improve
+                        performance of local governments in governance, administration, social and economic development and
+                        environmental management; Sustain peace and order condition and ensure public safety.</p>
+                </div>
+
             </div>
         </div>
-        <div class="text-center m-2 mb-4">
-            <h4 class="fw-bold">Our Slogan</h4>
-            <h5 class="fw-bold">Matino | Mahusay | Maaasahan</h5>
-        </div>
+    </div>
+
+    </div>
+    <div class="text-center m-2 mb-4 mt-2">
+        <h4 class="fw-bold">Our Slogan</h4>
+        <h5 class="fw-bold">Matino | Mahusay | Maaasahan</h5>
+    </div>
     </div>
 @endsection
