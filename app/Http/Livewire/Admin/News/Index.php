b614d963f9e83fbd68ca61b5e1a68fc35ed6cefd
viennarose <viennapepito01@gmail.com>
2023-02-03 10:55:59 +0800

    changed

--- app/Http/Livewire/Admin/News/Index.php
+++ app/Http/Livewire/Admin/News/Index.php
@@ -4,30 +4,51 @@ namespace App\Http\Livewire\Admin\News;
 
 use App\Models\News;
 use Livewire\Component;
+use Livewire\WithPagination;
 use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\File;
 
 class Index extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+
     use WithFileUploads;
     public $title, $caption, $image = [];
     public  $new_image = [], $old_image = [];
     public $news_id;
 
-    public function closeModal()
-    {
-        $this->resetInput();
-        $this->dispatchBrowserEvent('close-modal');
+    public $search;
+    public $fromDate = null;
+    public $toDate = null;
+
+
+    public function loadNews(){
+
+        $query = News::orderBy('title', 'asc')
+            ->search($this->search);
+
+        if($this->title){
+            $query->where('title', $this->title);
+        }
+        if(($this->fromDate && $this->toDate) != null){
+            $query->whereDate('datetime', '>=', $this->fromDate)
+                ->whereDate('datetime', '<=', $this->toDate);
+        }
+        $news = $query->paginate(5);
+        return compact('news');
     }
+
+
     public function addNews(){
         $this->validate([
             'title' => 'string',
             'caption' => 'string',
-            'image.*' => 'image|max:1024', // 1MB Max
+            'image.*' => 'image', // 1MB Max
         ]);
-
         foreach ($this->image as $key => $img) {
-            $this->image[$key] = $img->store('services', 'public');
+            $this->image[$key] = $img->store('images');
         }
 
         $this->image = json_encode($this->image);
@@ -42,8 +63,44 @@ class Index extends Component
         session()->flash('success', 'Images has been successfully Uploaded.');
     }
 
+    public function editNews(int $news_id)
+    {
+        $news = News::find($news_id);
+        if($news){
+            $this->news_id = $news->id;
+            $this->title = $news->title;
+            $this->caption = $news->caption;
 
+        }else{
+            return redirect()->to('/admin/news');
+        }
 
+    }
+    public function updateNews(){
+        $this->validate([
+            'title' => 'string',
+            'caption' => 'string',
+            'image.*' => 'image', // 1MB Max
+
+        ]);
+
+        foreach ($this->image as $key => $img) {
+            $this->image[$key] = $img->store('services', 'public');
+        }
+
+        $this->image = json_encode($this->image);
+
+        News::where('id',$this->news_id)->update([
+            'title' => $this->title,
+            'caption' => $this->caption,
+            'datetime' => $this->datetime = now(),
+            'image' => $this->image,
+        ]);
+
+        session()->flash('message','news Updated Successfully');
+        $this->resetInput();
+        $this->dispatchBrowserEvent('close-modal');
+    }
 
     public function deleteNews(int $news_id)
     {
@@ -53,18 +110,17 @@ class Index extends Component
     public function destroyNews()
     {
         $news = News::find($this->news_id);
-        $destination=public_path('storage\\'.$news->image);
+        $destination= public_path('/images/'.$news->image);
         if(File::exists($destination)){
             File::delete($destination);
         }
-
-        $result=$news->delete();
-        if ($result) {
-            session()->flash('Success', 'Delete Successfully');
-        } else {
-            session()->flash('error', 'Not Delete Successfully');
-        }
-        // session()->flash('message','Appointment Deleted Successfully');
+        $news->delete();
+        // if ($result) {
+        //     session()->flash('Success', 'Delete Successfully');
+        // } else {
+        //     session()->flash('error', 'Not Delete Successfully');
+        // }
+        session()->flash('message','News deleted Successfully');
         $this->dispatchBrowserEvent('close-modal');
     }
 
@@ -73,9 +129,15 @@ class Index extends Component
     {
         $this->title = '';
         $this->caption = '';
-        $this->datetime = '';
         $this->image = '';
     }
+
+    public function closeModal()
+    {
+        $this->resetInput();
+        $this->dispatchBrowserEvent('close-modal');
+    }
+
     public function render()
     {
         $news = News::get();
