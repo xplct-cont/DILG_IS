3743a968adc67363ddde57a234fe48bd17c11f86
xplct-cont <kennsecusana@gmail.com>
2023-02-03 13:21:25 +0800

    Added Change Password

---
+++ app/Http/Controllers/Admin_View/Admin_ChangePasswordController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin_View;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use File;
+use Illuminate\Support\Facades\Auth;
+use App\Rules\MatchOldPassword;
+use Illuminate\Support\Facades\Hash;
+
+class Admin_ChangePasswordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+
+        $change_pass = User::findOrFail($id);
+        return view('Admin_View.change_password.change_password', compact('change_pass'));
+    }
+
+    public function change_password(Request $request){
+          
+        $request->validate([
+            'current_password' => ['required', new MatchOldPassword],
+            'new_password' => ['required'],
+            'new_confirm_password' => ['same:new_password'],
+        ]);
+   
+        User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+   
+        return redirect()->back()->with('status', 'Password changed successfully!');
+     }
+}
