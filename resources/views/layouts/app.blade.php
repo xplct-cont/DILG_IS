b68435d1998f324a6e53a971cc3806abdd2b343c
xplct-cont <kennsecusana@gmail.com>
2023-02-03 11:22:08 +0800

    Changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -165,7 +165,7 @@
                                 </li>
                             @endif --}}
                         @else
-                            <li class="nav-item dropdown">
+                            {{-- <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -183,7 +183,7 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
+                            </li> --}}
                         @endguest
                     </ul>
                 </div>
