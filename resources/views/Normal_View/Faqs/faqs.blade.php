74e3c449f441dfc1769aa6d54c2466845b9aae44
viennarose <viennapepito01@gmail.com>
2023-02-13 15:31:04 +0800

    faq

--- resources/views/Normal_View/Faqs/faqs.blade.php
+++ resources/views/Normal_View/Faqs/faqs.blade.php
@@ -2,8 +2,54 @@
 @section('content')
 
 
+<div class="accordion" id="accordionExample">
+    @foreach ($faq as $fq )
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne{{$fq->id}}">
+          <button class="accordion-button bg-info" type="button"
+          data-bs-toggle="collapse" data-bs-target="#collapseOne{{$fq->id}}" aria-expanded="true" aria-controls="collapseOne">
+              <strong>{{$fq->questions}}<strong>
+          </button>
+        </h2>
+        <div id="collapseOne{{$fq->id}}" class="accordion-collapse collapse show"
+        aria-labelledby="headingOne{{$fq->id}}" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            {{$fq->answers}}
+          </div>
+        </div>
+      </div>
+    @endforeach
 
- <p class="text-center">Under Construction !!!</p>
+    {{-- <div class="accordion-item">
+      <h2 class="accordion-header" id="headingTwo">
+        <button class="accordion-button collapsed bg-info" type="button"
+        data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <strong><strong>
+        </button>
+      </h2>
+      <div id="collapseTwo" class="accordion-collapse collapse"
+      aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingThree">
+        <button class="accordion-button collapsed bg-info" type="button"
+        data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            <strong><strong>
+        </button>
+      </h2>
+      <div id="collapseThree" class="accordion-collapse collapse"
+      aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+
+        </div>
+      </div>
+    </div> --}}
+</div>
+
 
 
 @endsection
