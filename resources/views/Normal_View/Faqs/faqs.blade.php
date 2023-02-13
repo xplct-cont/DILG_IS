a66dd31a7baa4d80b17867415e85666fe97a7c93
viennarose <viennapepito01@gmail.com>
2023-02-13 15:36:47 +0800

    grer

--- resources/views/Normal_View/Faqs/faqs.blade.php
+++ resources/views/Normal_View/Faqs/faqs.blade.php
@@ -1,55 +1,28 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="col-md-12">
 
 <div class="accordion" id="accordionExample">
     @foreach ($faq as $fq )
     <div class="accordion-item">
-        <textarea class="accordion-header" id="headingOne{{$fq->id}}">
+        <h2 class="accordion-header" id="headingOne{{$fq->id}}">
           <button class="accordion-button bg-info" type="button"
           data-bs-toggle="collapse" data-bs-target="#collapseOne{{$fq->id}}" aria-expanded="true" aria-controls="collapseOne">
               <strong>{{$fq->questions}}<strong>
           </button>
-        </textarea>
+        </h2>
         <div id="collapseOne{{$fq->id}}" class="accordion-collapse collapse show"
         aria-labelledby="headingOne{{$fq->id}}" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <textarea name="" id="" cols="30" rows="10">{{$fq->answers}}</textarea>
+          <div class="accordion-body text-wrap">
+            {{$fq->answers}}
           </div>
         </div>
       </div>
     @endforeach
 
-    {{-- <div class="accordion-item">
-      <h2 class="accordion-header" id="headingTwo">
-        <button class="accordion-button collapsed bg-info" type="button"
-        data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            <strong><strong>
-        </button>
-      </h2>
-      <div id="collapseTwo" class="accordion-collapse collapse"
-      aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="headingThree">
-        <button class="accordion-button collapsed bg-info" type="button"
-        data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            <strong><strong>
-        </button>
-      </h2>
-      <div id="collapseThree" class="accordion-collapse collapse"
-      aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-
-        </div>
-      </div>
-    </div> --}}
 </div>
-
+</div>
 
 
 @endsection
