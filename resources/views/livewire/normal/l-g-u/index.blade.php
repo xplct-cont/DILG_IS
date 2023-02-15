d05a8ce098bcb531125f185ad8c4ccbac43c2fa0
viennarose <viennapepito01@gmail.com>
2023-02-15 10:37:59 +0800

    filter lgus

--- resources/views/livewire/normal/l-g-u/index.blade.php
+++ resources/views/livewire/normal/l-g-u/index.blade.php
@@ -1,13 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="text-center mt-3 mb-4">
-        <img src="/img/bohol_seal.png" class="elevation-4" style="border-radius: 50%; height: 200px; width: 200px;"
-            alt="">
-    </div>
-
-    <livewire:normal.l-g-u.index>
-{{--
+<div>
     <div class="container">
         <div class="row">
             <div class="col-md-6" style=" font-weight: 400; color:#C9282D;">
@@ -53,6 +44,24 @@
         </div>
     </div>
 
+    <div class="container mb-4">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6 mt-2">
+                    <select class="form-select mt-1"  wire:model="municipality">
+                        <option value="all">All municipalities</option>
+                        @foreach ($municipalities as $municipality)
+                            <option value="{{ $municipality->id }}">{{ $municipality->municipality }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-6 mt-2">
+                    <input type="search" style="border-radius: 20px;" wire:model="search" class="form-control input"
+                        placeholder="Search">
+                </div>
+            </div>
+        </div>
+    </div>
     <p class="text-center mt-3" style="color:dimgray; font-size: 22px; font-weight: 600;">LIST OF LOCAL ELECTIVE OFFICIALS
     </p>
     <div class="col-md-12">
@@ -104,25 +113,5 @@
 
             </div>
         </div>
-    </div> --}}
-@endsection
-
-
-<style scoped>
-    /* .card {
-        --bg-color: #DCE9FF;
-        --bg-color-light: #f1f7ff;
-        --text-color-hover: whitesmoke;
-        --box-shadow-color: silver;
-    }
-
-    .card:hover {
-        transform: translateY(-5px) scale(1.005) translateZ(0);
-        box-shadow: 0 24px 36px silver,
-            0 24px 46px var(--box-shadow-color);
-    }
-
-    .card:hover .overlay {
-        transform: scale(8) translateZ(0);
-    } */
-</style>
+    </div>
+</div>
