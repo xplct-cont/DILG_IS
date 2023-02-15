5fcc4c8dd5c2f475c2ef30ccd809dce401a1a3ac
xplct-cont <kennsecusana@gmail.com>
2023-02-15 16:44:21 +0800

    Changed

--- resources/views/Normal_View/Bohol_Issuances/show.blade.php
+++ resources/views/Normal_View/Bohol_Issuances/show.blade.php
@@ -25,7 +25,7 @@
                             style="font-size: 15px; font-weight: 350; color:rgb(77, 77, 77);">{{ $b_issuance->reference_num }}</span>
                     </p>
                     <div class="mx-auto text-center">
-                        <iframe class="responsive mb-3 " src= {{ "/app/public/issuance_files/".$b_issuance->file}}
+                        <iframe class="responsive mb-3 " src="{{ asset('/app/public/issuance_files/' . $b_issuance->file) }}"
                             width="90%" height="500px;">
                         </iframe>
                     </div>
