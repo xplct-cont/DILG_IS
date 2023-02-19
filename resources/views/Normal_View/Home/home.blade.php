17107b72fffb083785ed37995283360f75c3b8e7
xplct-cont <kennsecusana@gmail.com>
2023-02-19 16:21:18 +0800

    Added News Single ID View

--- resources/views/Normal_View/Home/home.blade.php
+++ resources/views/Normal_View/Home/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-   
     <div class="mx-auto text-center">
         <div class="">
             @foreach ($home_images as $home_img)
@@ -78,48 +77,7 @@
     <div class="mx-auto">
         <div class="col-md-12">
             <div class="row">
-                {{-- @foreach ($projectsAll as $proj)
-                    <div class="card col-md-5 m-1 mx-auto">
-                        <div class="card-body  text-center">
-                            <div class="card-header" style="background-color:#C9282D; color:white;">
-                                <h1 style="font-size: 20px;" class="text-center">Province of {{ $proj->province }}</h1>
-                            </div>
-                            <p class=" text-justify mt-3" style="font-weight: 500; font-size: 16px;">Program: <span
-                                    style="font-size: 16px; font-weight: 400;"> {{ $proj->program->title }}</span></p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">Project
-                                Code: <span style="font-size: 16px; font-weight: 400;"> {{ $proj->proj_code }}</span></p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">Title: <span
-                                    style="font-size: 16px; font-weight: 400;"> {{ Illuminate\Support\Str::limit($proj->title, 40) }}</span></p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">
-                                Municipality: <span style="font-size: 16px; font-weight: 400;">
-                                    {{ $proj->municipality->municipality }}</span>
-                            </p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">Exact
-                                Location: <span style="font-size: 16px; font-weight: 400;"> {{ $proj->exact_loc }}</span></p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">Type: <span
-                                    style="font-size: 16px; font-weight: 400;"> {{ $proj->type }}</span></p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">Year:
-                                <span style="font-size: 16px; font-weight: 400;"> {{ $proj->year }}</span>
-                            </p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">Status:
-                                <span style="font-size: 16px; font-weight: 400;"> {{ $proj->status }}</span>
-                            </p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">Total
-                                Cost:
-                                <span style="font-size: 16px; font-weight: 400;"> {{ $proj->total_cost }}</span>
-                            </p>
-                            <p class=" text-justify " style="margin-top: -12px; font-weight: 500; font-size: 16px;">
-                                Description:
-                                <span style="font-size: 16px; font-weight: 400;">{{ Illuminate\Support\Str::limit($proj->description, 30) }}</span>
-                            </p>
-    
-                            <iframe class="col-md-12"
-                            src="{{ $proj->municipality->gmap_url}}"
-                            height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade" class="rounded"></iframe>
-                        </div>
-                    </div>
-                @endforeach --}}
+
                 @foreach ($projectsAll as $proj)
                     <div class="card col-md-5 m-1 mx-auto">
                         <div class="card-body ">
@@ -274,49 +232,104 @@
 
     <div class="mx-auto mt-3 col-md-11">
         <div class="">
-
             @foreach ($news_images as $new)
-                <a href="#" style="text-decoration: none; color:#030303;">
-                    <div class=" card elevation-4">
-                        <div class="row d-flex justify-content-center mt-3 ml-3 mr-3  ">
-                            <div class="col-md-6 mx-auto mt-5 ">
+                <div class="card">
+                    <div class=" card-body elevation-4">
+                        <a href="#" data-toggle="modal" id="news_updates_view_link"
+                            data-target="#news_update_id{{ $new->id }}" style="text-decoration:none; color:dimgray">
 
-                                <h1 style="font-size: 18px; font-weight: 500; color:dimgray;">
-                                    Post: {{ \Carbon\Carbon::parse($new->created_at)->format('M j, Y h:i A') }}</h1><br>
-                                <p class="text-left" style="font-size: 18px;">{{ $new->title }}</p>
-                                <p class="text-left" style="font-size: 15px; font-weight: 400;">
-                                    {{ Illuminate\Support\Str::limit($new->caption, 500) }}</p>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="col-md-12" style="height: 365px;">
+                            <div class="row d-flex justify-content-center mt-3">
+                                <div class="col-md-6 mx-auto mt-5 ">
 
-                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                        <ol class="carousel-indicators">
-                                            <li data-target="#carouselExampleIndicators" data-slide-to="0"
-                                                class="active"></li>
-                                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                    <h1 style="font-size: 18px; font-weight: 500; color:dimgray;">
+                                        Post: {{ \Carbon\Carbon::parse($new->created_at)->format('M j, Y h:i A') }}</h1>
+                                    <br>
+                                    <p class="text-left" style="font-size: 18px;">{{ $new->title }}</p>
+                                    <p class="text-left" style="font-size: 15px; font-weight: 400;">
+                                        {{ Illuminate\Support\Str::limit($new->caption, 500) }}</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="" style="height: 360px;">
 
-                                        </ol>
-                                        <div class=" carousel-inner text-center" style="height:350px;">
-                                            @php
-                                                $images = json_decode($new->images, true);
-                                            @endphp
-                                            @foreach ($images as $key => $image)
-                                                <div class="carousel-item zoom {{ $key == 0 ? 'active' : '' }}">
-                                                    <img src="{{ asset('/news_updates/' . $image) }}"
-                                                        style="height:350px; width:100%;" />
-                                                </div>
-                                            @endforeach
+                                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                            <ol class="carousel-indicators">
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="0"
+                                                    class="active">
+                                                </li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                            </ol>
+                                            <div class="carousel-inner text-center" style="height:350px;">
+                                                @php
+                                                    $images = json_decode($new->images, true);
+                                                @endphp
+                                                @foreach ($images as $key => $image)
+                                                    <div class="carousel-item zoom {{ $key == 0 ? 'active' : '' }}">
+                                                        <img src="{{ asset('/news_updates/' . $image) }}"
+                                                            style="height:350px; width:100%;" />
+                                                    </div>
+                                                @endforeach
+
+                                            </div>
 
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                </a>
 
+                <div class="modal fade" id="news_update_id{{ $new->id }}" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header ">
+
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body" style="color:dimgray;">
+
+                                <div class="">
+                                    <h1 class="text-end" style="font-size: 18px; font-weight: 500; color:dimgray;">
+                                        Post: {{ \Carbon\Carbon::parse($new->created_at)->format('M j, Y h:i A') }}</h1>
+                                    <br>
+                                    <p class="text-left" style="font-size: 18px;">{{ $new->title }}</p>
+                                    <p class="text-left" style="font-size: 15px; font-weight: 400;">
+                                        {{ $new->caption }}</p>
+
+                                    <div class="" style="height: 365px;">
+
+                                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                            <ol class="carousel-indicators">
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="0"
+                                                    class="active"></li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+
+                                            </ol>
+                                            <div class=" carousel-inner text-center" style="height:350px;">
+                                                @php
+                                                    $images = json_decode($new->images, true);
+                                                @endphp
+                                                @foreach ($images as $key => $image)
+                                                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
+                                                        <img src="{{ asset('/news_updates/' . $image) }}"
+                                                            style="height:350px; width:100%;" />
+                                                    </div>
+                                                @endforeach
+
+                                            </div>
+
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </a>
+                </div>
             @endforeach
         </div>
     </div>
@@ -377,7 +390,7 @@
         /* IE 9 */
         -webkit-transform: scale(1.2);
         /* Safari 3-8 */
-        transform: scale(1.2);
+        transform: scale(0.9);
         text-align: center;
         font-size: 10px;
     }
@@ -394,11 +407,11 @@
         --box-shadow-color: silver;
     }
 
-    .card:hover {
+    /* .card:hover {
         transform: translateY(-5px) scale(1.005) translateZ(0);
         box-shadow: 0 24px 36px silver,
             0 24px 46px var(--box-shadow-color);
-    }
+    } */
 
     .card:hover .overlay {
         transform: scale(8) translateZ(0);
--- resources/views/Normal_View/Updates/index.blade.php
+++ resources/views/Normal_View/Updates/index.blade.php
@@ -23,49 +23,103 @@
 
     <div class="mx-auto mt-3 col-md-11">
         <div class="">
-
             @foreach ($news_images as $new)
-                <a href="#" style="text-decoration: none; color:#030303;">
-                    <div class=" card elevation-4">
-                        <div class="row d-flex justify-content-center mt-3 ml-3 mr-3  ">
-                            <div class="col-md-6 mx-auto mt-5 ">
+                <div class="card">
+                    <div class=" card-body elevation-4">
+                        <a href="#" data-toggle="modal" id="news_updates_view_link"
+                            data-target="#news_update_id{{ $new->id }}" style="text-decoration:none; color:dimgray">
 
-                                <h1 style="font-size: 18px; font-weight: 500; color:dimgray;">
-                                    Post: {{ \Carbon\Carbon::parse($new->created_at)->format('M j, Y h:i A') }}</h1><br>
-                                <p class="text-left" style="font-size: 18px;">{{ $new->title }}</p>
-                                <p class="text-left" style="font-size: 15px; font-weight: 400;">
-                                    {{ Illuminate\Support\Str::limit($new->caption, 500) }}</p>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="col-md-12" style="height: 360px;">
+                            <div class="row d-flex justify-content-center mt-3">
+                                <div class="col-md-6 mx-auto mt-5 ">
 
-                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                        <ol class="carousel-indicators">
-                                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
-                                            </li>
-                                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                    <h1 style="font-size: 18px; font-weight: 500; color:dimgray;">
+                                        Post: {{ \Carbon\Carbon::parse($new->created_at)->format('M j, Y h:i A') }}</h1><br>
+                                    <p class="text-left" style="font-size: 18px;">{{ $new->title }}</p>
+                                    <p class="text-left" style="font-size: 15px; font-weight: 400;">
+                                        {{ Illuminate\Support\Str::limit($new->caption, 500) }}</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="" style="height: 360px;">
 
-                                        </ol>
-                                        <div class="carousel-inner text-center" style="height:350px;">
-                                            @php
-                                                $images = json_decode($new->images, true);
-                                            @endphp
-                                            @foreach ($images as $key => $image)
-                                                <div class="carousel-item zoom {{ $key == 0 ? 'active' : '' }}">
-                                                    <img src="{{ asset('/news_updates/' . $image) }}"
-                                                        style="height:350px; width:100%;" />
-                                                </div>
-                                            @endforeach
+                                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                            <ol class="carousel-indicators">
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="0"
+                                                    class="active">
+                                                </li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+
+                                            </ol>
+                                            <div class="carousel-inner text-center" style="height:350px;">
+                                                @php
+                                                    $images = json_decode($new->images, true);
+                                                @endphp
+                                                @foreach ($images as $key => $image)
+                                                    <div class="carousel-item zoom {{ $key == 0 ? 'active' : '' }}">
+                                                        <img src="{{ asset('/news_updates/' . $image) }}"
+                                                            style="height:350px; width:100%;" />
+                                                    </div>
+                                                @endforeach
+
+                                            </div>
 
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                </a>
 
+                <div class="modal fade" id="news_update_id{{ $new->id }}" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header ">
+
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body" style="color:dimgray;">
+
+                                <div class="">
+                                    <h1 class="text-end" style="font-size: 18px; font-weight: 500; color:dimgray;">
+                                        Post: {{ \Carbon\Carbon::parse($new->created_at)->format('M j, Y h:i A') }}</h1><br>
+                                    <p class="text-left" style="font-size: 18px;">{{ $new->title }}</p>
+                                    <p class="text-left" style="font-size: 15px; font-weight: 400;">
+                                        {{ $new->caption }}</p>
+
+                                    <div class="" style="height: 365px;">
+
+                                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                            <ol class="carousel-indicators">
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="0"
+                                                    class="active"></li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+
+                                            </ol>
+                                            <div class=" carousel-inner text-center" style="height:350px;">
+                                                @php
+                                                    $images = json_decode($new->images, true);
+                                                @endphp
+                                                @foreach ($images as $key => $image)
+                                                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
+                                                        <img src="{{ asset('/news_updates/' . $image) }}"
+                                                            style="height:350px; width:100%;" />
+                                                    </div>
+                                                @endforeach
+
+                                            </div>
+
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </a>
+                </div>
             @endforeach
         </div>
     </div>
