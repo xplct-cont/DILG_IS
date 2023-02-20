3c992ed7ee8ec0759928b39aa19238a9c3e40938
xplct-cont <kennsecusana@gmail.com>
2023-02-20 10:50:51 +0800

    Added Limits

--- app/Http/Controllers/Normal_View/Bohol_Issuances/Bohol_IssuancesController.php
+++ app/Http/Controllers/Normal_View/Bohol_Issuances/Bohol_IssuancesController.php
@@ -12,7 +12,7 @@ class Bohol_IssuancesController extends Controller
 {
     public function index(){
         
-        $issuances = Bohol_Issuance::orderBy('date', 'DESC')->get();
+        $issuances = Bohol_Issuance::orderBy('date', 'ASC')->limit(6)->get();
         return view('Normal_View.Bohol_Issuances.bohol_issuances', compact('issuances'));
     }
 
@@ -20,7 +20,7 @@ class Bohol_IssuancesController extends Controller
     public function show( $id){
 
         $b_issuance = Bohol_Issuance::find($id);
-        $issuances = Bohol_Issuance::all();
+        $issuances = Bohol_Issuance::orderBy('date', 'ASC')->limit(3)->get();;
 
     
         return view('Normal_View.Bohol_Issuances.show', compact('b_issuance', 'issuances'));
