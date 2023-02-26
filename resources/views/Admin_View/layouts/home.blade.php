718603877860d251bd9bebf390f5f98596e73d1b
xplct-cont <kennsecusana@gmail.com>
2023-02-26 10:20:54 +0800

    Added js timeout function

--- resources/views/Admin_View/layouts/home.blade.php
+++ resources/views/Admin_View/layouts/home.blade.php
@@ -1,7 +1,7 @@
 @extends('Admin_View.layouts.app')
 @section('content')
     @if ($message = Session::get('message'))
-        <div class="alert alert-success alert-block mt-2">
+        <div class="alert alert-success alert-dismissible fade show mt-2" role="alert">
             <button type="button" class="close" data-dismiss="alert" style="color:black;">×</button>
             <strong>{{ $message }}</strong>
         </div>
@@ -302,13 +302,18 @@
         </div>
     </section>
     </div>
+
+    <script>
+        setTimeout(function() {
+            $(' .alert-dismissible').fadeOut('slow');
+        }, 3000);
+    </script>
 @endsection
 
 <style scoped>
     .img {
         display: flex;
         justify-content: space-evenly;
-
 
     }
 </style>
