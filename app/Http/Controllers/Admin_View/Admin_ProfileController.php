b9597fefcf563dff3e4ede0843817288569b29db
xplct-cont <kennsecusana@gmail.com>
2023-02-26 09:34:57 +0800

    Changed prof

--- app/Http/Controllers/Admin_View/Admin_ProfileController.php
+++ app/Http/Controllers/Admin_View/Admin_ProfileController.php
@@ -79,10 +79,10 @@ class Admin_ProfileController extends Controller
 
         if($request->hasFile('profile_image')){
       
-            $destination = 'user_profile_images/'.$profile->profile_image;
-            if(File::exists($destination)){
-                File::delete($destination);
-            }
+            // $destination = 'user_profile_images/'.$profile->profile_image;
+            // if(File::exists($destination)){
+            //     File::delete($destination);
+            // }
 
             $file = $request->file('profile_image');
             $extention = $file->getClientOriginalExtension();
