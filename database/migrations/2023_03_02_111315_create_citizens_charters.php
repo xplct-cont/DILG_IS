7496c1ccfd755ed065a7ee55cf4121cea213e164
xplct-cont <kennsecusana@gmail.com>
2023-03-02 11:18:54 +0800

    Added citizens charter migrations table

---
+++ database/migrations/2023_03_02_111315_create_citizens_charters.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citizens_charters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('page_num')->nullable();
+            $table->longText('images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citizens_charters');
+    }
+};
