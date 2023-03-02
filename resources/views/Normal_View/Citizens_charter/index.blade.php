8ca5de1fd15436f57c177343ae2974b9c610f9c3
xplct-cont <kennsecusana@gmail.com>
2023-03-02 14:12:28 +0800

    Added

--- resources/views/Normal_View/Citizens_charter/index.blade.php
+++ resources/views/Normal_View/Citizens_charter/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 @section('content')
-    <h1 class="text-center mt-5 " style="font-size: 30px; margin-bottom: 50px;">CITIZENS CHARTER</h1>
+    <div class="d-flex justify-content-center mb-3">
+        <img src="/img/dilg-main.png" style="height: 150px; width: 150px;" alt="">
+    </div>
+    <h1 class="text-center mt-2" style="font-size: 28px; margin-bottom: 50px;">CITIZENS CHARTER</h1>
 
     <div class="container">
-
         @foreach ($cit_charter as $citizen)
             <div class="bg-secondary p-1 mr-3 ml-3" style="">
                 <p class="text-wrap text-center mt-1" style="font-weight: 500; font-size: 18px;">{{ $citizen->title }}</p>
