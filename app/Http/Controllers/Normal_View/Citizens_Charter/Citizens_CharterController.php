3a608dd84d73fdc579d4976e37b9cc5aee284cb4
xplct-cont <kennsecusana@gmail.com>
2023-03-08 12:52:21 +0800

    set time limit

--- app/Http/Controllers/Normal_View/Citizens_Charter/Citizens_CharterController.php
+++ app/Http/Controllers/Normal_View/Citizens_Charter/Citizens_CharterController.php
@@ -17,8 +17,11 @@ class Citizens_CharterController extends Controller
     }
 
     public function export_citizes_charter_pdf(){
+
+        set_time_limit(60);
+        
         $cit_charter = Citizens_Charter::whereNotNull('created_at')->orderBy('created_at', 'ASC')->get();
-        $pdf = PDF::loadVIew('pdf.citizens_charters', [
+        $pdf = PDF::loadView('pdf.citizens_charters', [
             'citizens_charters' => $cit_charter
         ]);
         return $pdf->download('Citizens Charter.pdf');
