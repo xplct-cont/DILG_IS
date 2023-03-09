1be1adf4bd1edfb5e3a835d1699cf7906035dbd2
viennarose <viennapepito01@gmail.com>
2023-03-09 19:41:01 +0800

    logs

--- app/Models/Updates_Image.php
+++ app/Models/Updates_Image.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Updates_Image extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+    protected $guarded = [];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly(['images'])
+        ->setDescriptionForEvent(fn(string $eventName) => "The Updates Image has been {$eventName}")
+        ->logOnlyDirty();
+    }
 }
