dcad8dfefb04c7f93a32673d246b5d08d85792c1
viennarose <viennapepito01@gmail.com>
2023-03-27 11:26:20 +0800

    last

--- resources/views/Admin_View/updates/index.blade.php
+++ resources/views/Admin_View/updates/index.blade.php
@@ -270,7 +270,7 @@
 
                                                 @role('Super-Admin')
                                                     <div class="text-start mb-3">
-                                                        @if ($news_img->status === false)
+                                                        @if ($news_img->status == false)
                                                             <span>
                                                                 <form action="{{ url('/approve/' . $news_img->id) }}"
                                                                     method="POST" class="d-inline-block">
@@ -280,7 +280,7 @@
                                                                         ?</button>
                                                                 </form>
                                                             </span>
-                                                        @elseif ($news_img->status === true)
+                                                        @elseif ($news_img->status == true)
                                                             <span>
                                                                 <form action="{{ url('/disapprove/' . $news_img->id) }}"
                                                                     method="POST" class="d-inline-block">
