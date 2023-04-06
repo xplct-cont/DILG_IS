ef7292e120205f05619fe8d9e10ac001db4fcef2
Kenn Secusana <kennsecusana@gmail.com>
2023-04-06 11:44:46 +0800

    Changed

--- resources/views/Admin_View/downloadables/downloadables.blade.php
+++ resources/views/Admin_View/downloadables/downloadables.blade.php
@@ -105,7 +105,7 @@
                                             <label for="" style="color:dimgray">You may also upload PDF File (Max: 1
                                                 File):
                                             </label>
-                                            <input type="file" class="form-control" name="file" >
+                                            <input type="file" class="form-control" name="file" required>
                                             @if ($errors->has('file'))
                                                 {{ $errors->first('file') }}
                                             @endif
