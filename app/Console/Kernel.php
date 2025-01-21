c4b4351b36f9dfbfc4739ab968bbe22ee4728fe1
VnceMari <vinsmar8888@gmail.com>
2025-01-21 10:40:46 +0800

    automatic scraping once a day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('scrape:legalopinions')->daily(); // Set to daily or any frequency you prefer
     }
 
     /**
