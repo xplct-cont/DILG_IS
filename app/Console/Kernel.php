600cd016d195299a9dd918c4ad1dc225ca523ce6
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-01-30 13:37:00 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Services\ScraperService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +17,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $scraperService = app(ScraperService::class);
+            $url = 'https://dilg.gov.ph/legal-opinions-archive/';
+            $scraperService->scrapeLegalOpinions($url);
+        })->everyMinute();
+
     }
 
     /**
