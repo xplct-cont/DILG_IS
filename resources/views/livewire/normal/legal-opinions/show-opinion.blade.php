41839b1aef643ee980f76243633816d7e8da920c
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-02-03 15:17:06 +0800

    fixed changes for the legal opinions

--- resources/views/livewire/normal/legal-opinions/show-opinion.blade.php
+++ resources/views/livewire/normal/legal-opinions/show-opinion.blade.php
@@ -21,15 +21,19 @@
                     </div>
                     <div class="col-md-12">
                         <div class="overflow-x-auto bg-white mt-4" style="padding: 15px; padding-left: 25px; border:1px solid rgb(201, 201, 201);">
-                            <strong>Reference Number:</strong> <span>{{ $opinion->reference }}</span>
+                            <strong>Reference Number:</strong> 
+                            <span>
+                                {{ Str::replaceFirst('Category:', '', $opinion->reference) }}
+                            </span>
                         </div>
+                        
 
                          <div class="mt-4">
                             <iframe src="{{ $opinion->download_link }}" width="100%" height="500px" style="border: 1px solid #ccc;"></iframe>
                         </div>
 
                         <div class="mt-4">
-                            <a href="{{ asset('storage/' . $opinion->attachment) }}" style="text-decoration: none;">
+                            <a href="{{ $opinion->download_link }}" target="_blank" style="text-decoration: none;">
                                 <span class="p-2" style="border:1px solid rgb(189, 188, 188); color:rgb(79, 79, 79);">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-file-earmark-pdf" viewBox="0 0 16 16">
                                         <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
