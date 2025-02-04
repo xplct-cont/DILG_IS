ab8e3802f8eb388c45857208dc731ae297ef0831
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-02-04 09:32:36 +0800

    proxy use of pdf viewer

--- resources/views/livewire/normal/legal-opinions/show-opinion.blade.php
+++ resources/views/livewire/normal/legal-opinions/show-opinion.blade.php
@@ -27,9 +27,9 @@
                             </span>
                         </div>
                         
-
                          <div class="mt-4">
-                            <iframe src="{{ $opinion->download_link }}" width="100%" height="500px" style="border: 1px solid #ccc;"></iframe>
+                            <iframe src="/proxy/pdf?url={{ urlencode($opinion->download_link) }}" width="100%" height="1000px"></iframe>
+
                         </div>
 
                         <div class="mt-4">
