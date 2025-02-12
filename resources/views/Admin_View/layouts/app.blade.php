45f1d9d08b29d9dd771d7db35990f1b1b6ca7117
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-02-12 09:10:42 +0800

    app name change

--- resources/views/Admin_View/layouts/app.blade.php
+++ resources/views/Admin_View/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }}</title>
+    <title>DILG Bohol Province</title>
    
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
