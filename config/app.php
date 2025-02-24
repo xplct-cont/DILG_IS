ca5d256f531eb51cecfe11f54a4597d284f54fbc
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-02-24 15:18:37 +0800

    intervention image removal

--- config/app.php
+++ config/app.php
@@ -196,7 +196,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Intervention\Image\ImageServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
@@ -252,7 +251,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'Excel' => \MaatWebsite\Excel\Facades\Excel::class,
         'Chatify' => Chatify\Facades\ChatifyMessenger::class,
