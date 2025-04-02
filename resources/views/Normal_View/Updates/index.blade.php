418fff532c1d3934e5dbb9616368cb6ef29505f9
VnceMari <vinsmar8888@gmail.com>
2025-04-02 10:59:12 +0800

    error fix for RA, pres directives, and update index

--- resources/views/Normal_View/Updates/index.blade.php
+++ resources/views/Normal_View/Updates/index.blade.php
@@ -37,7 +37,7 @@
     <div class="header ml-3 mr-3 mt-2" style=" padding: 7px;background: #234495; color: #FFFFFF; margin-bottom: 5px;">
         <h1 class="text-center" style="font-size: 18px; margin-top: 10px;">NEWS & UPDATES</h1>
     </div>
-    <livewire:normal.updates.index>
+    <livewire:normal.updates.index/>
 
 @endsection
 
